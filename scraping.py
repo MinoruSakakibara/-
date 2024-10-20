5f26a61666eb7c65d75ff251a445cb42d999df20
MinoruSakakibara <90542427+MinoruSakakibara@users.noreply.github.com>
2024-10-20 16:39:01 +0900

    エクセルの列がズレる問題を修正、処理終了時のメッセージウィンドウ追加

--- scraping.py
+++ scraping.py
@@ -6,15 +6,18 @@ import signal
 import tkinter as tk
 import pandas as pd
 from tkinter import ttk
+from tkinter import messagebox
 
 year = ''
 month = ''
 
+# メイン処理
 def update():
     year = entry_year.get()
     year = str(year)
     month = combobox.get()
 
+    # Webページ接続
     def scr():
         options = webdriver.ChromeOptions()
         options.set_capability("browserVersion", "113")
@@ -24,60 +27,50 @@ def update():
         driver.implicitly_wait(10)
         driver.get('https://kakaku.com/')
 
-        try:
+        # try:
             
-            btnGame = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div/div[4]/div/div/ul[7]/li[2]/a/p')
-            btnGame.click()
+        btnGame = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div/div[4]/div/div/ul[7]/li[2]/a/p')
+        btnGame.click()
+        
+        btnSchedule = driver.find_element(By.XPATH, '//*[@id="game"]/div[2]/div[2]/div[1]/ul/li[5]/dl/dd/a')
+        btnSchedule.click()
+        
+        dropdown = driver.find_element(By.ID, 'year')
+        dropdown_select = Select(dropdown)
+        dropdown_select.select_by_value(year)
+        
+        btnMonth = driver.find_element(By.LINK_TEXT, month)
+        btnMonth.click()
+        
+        # テーブル取得
+        table = driver.find_element(By.XPATH, '//*[@id="titleSche"]')
+        
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        
+        data = []
+        
+        for row in rows:
+            cols = row.find_elements(By.TAG_NAME, 'td')
+            cols =  [col.text for col in cols]
             
-            btnSchedule = driver.find_element(By.XPATH, '//*[@id="game"]/div[2]/div[2]/div[1]/ul/li[5]/dl/dd/a')
-            btnSchedule.click()
+            if len(cols) == 4:
+                cols.insert(0, "")
             
-            dropdown = driver.find_element(By.ID, 'year')
-            dropdown_select = Select(dropdown)
-            dropdown_select.select_by_value(year)
+            data.append(cols)
+        
+        # データフレーム作成、エクセル出力
+        df = pd.DataFrame(data)
+        
+        df.to_excel(year + '月' + month + '.xlsx', index=False, header=False)
+        
+        messagebox.showinfo('確認', '処理が終了しました')
             
-            btnMonth = driver.find_element(By.LINK_TEXT, month)
-            btnMonth.click()
-            
-            table = driver.find_element(By.XPATH, '//*[@id="titleSche"]')
-            
-            rows = table.find_elements(By.TAG_NAME, 'tr')
-            
-            data = []
-            
-            for row in rows:
-                cols = row.find_elements(By.TAG_NAME, 'td')
-                cols =  [col.text for col in cols]
-                data.append(cols)
-            
-            df = pd.DataFrame(data)
-            
-            for i in range(len(df)):
-                
-                head = df.iloc[i, 0]
-                
-                if head == 'PS5':
-                    df.shift(axis=1)
-                elif head == 'PS4':
-                    df.shift(axis=1)
-                elif head == 'Switch':
-                    df.shift(axis=1)
-                elif head == 'XSX':
-                    df.shift(axis=1)
-                
-            df.to_excel(year + '月' + month + '.xlsx', index=False, header=False)
-            
-            # element = driver.find_element(By.XPATH, '//*[@id="titleSche"]')
-            
-            # trlist = element.find_elements(By.TAG_NAME, 'tr')
-            
-            # for elem in trlist:
-            #     print(elem.text)
-            
-        finally:
-            os.kill(driver.service.process.pid,signal.SIGTERM)
+        # finally:
+        #     os.kill(driver.service.process.pid,signal.SIGTERM)
+        
     scr()
 
+# 入力ウィンドウ作成
 root = tk.Tk()
 root.title('ゲームソフト価格調査')
 root.geometry('300x150')
@@ -100,27 +93,3 @@ button1 = tk.Button(root, text='実行', command=update)
 button1.pack(pady=2)
 
 root.mainloop()
-
-# def scr():
-#     options = webdriver.ChromeOptions()
-#     options.set_capability("browserVersion", "113")
-#     # options.add_argument('--headless')
-
-#     driver = webdriver.Chrome(options = options)
-#     driver.implicitly_wait(10)
-#     driver.get('https://kakaku.com/')
-
-#     try:
-        
-#         btnGame = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div/div[4]/div/div/ul[7]/li[2]/a/p')
-#         btnGame.click()
-        
-#         btnSchedule = driver.find_element(By.XPATH, '//*[@id="game"]/div[2]/div[2]/div[1]/ul/li[5]/dl/dd/a')
-#         btnSchedule.click()
-        
-#         dropdown = driver.find_element(By.ID, 'year')
-#         dropdown_select = Select(dropdown)
-#         dropdown_select.select_by_value(year)
-
-#     finally:
-#         os.kill(driver.service.process.pid,signal.SIGTERM)
